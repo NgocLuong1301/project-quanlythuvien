b576310025752ecf6ae8b1fbb5e69b6dd6e1e412
Luong <24106440@st.phenikaa-uni.edu.vn>
2026-07-04 00:41:05 +0700

    Update MySQL connection

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "library_management"
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "library_management",
 });
 
-db.connect((err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Connected MySQL");
-    }
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected MySQL Successfully");
+  }
 });
 
 module.exports = db;
